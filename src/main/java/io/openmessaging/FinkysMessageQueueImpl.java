3f5babda29923db83d44c9426234c5d954321ed0
彭智耿 <pengzhigeng@sfmail.sf-express.com>
2021-09-18 18:04:42 +0800

    暂存 翻屋企

---
+++ src/main/java/io/openmessaging/FinkysMessageQueueImpl.java
@@ -0,0 +1,184 @@
+package io.openmessaging;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
+
+import sun.misc.Cleaner;
+
+/**
+ * 这是一个简单的基于内存的实现，以方便选手理解题意；
+ * 实际提交时，请维持包名和类名不变，把方法实现修改为自己的内容；
+ */
+public class FinkysMessageQueueImpl extends MessageQueue {
+
+    public static final String DIR_PMEM = "/pmem";
+    public static final Path DIR_ESSD = Paths.get("/essd");
+    //    public static final Path DIR_ESSD = Paths.get(System.getProperty("user.dir")).resolve("target");
+    public static final ConcurrentHashMap<String, AtomicLong> APPEND_OFFSET_MAP = new ConcurrentHashMap<>();
+    public static FileChannel WAL;
+
+
+    public static final int WAL_COUNT = 10;
+    public static FileChannel[] WAL_FILE_CHANNELS = new FileChannel[WAL_COUNT];
+    static {
+        try {
+            for (int i = 0; i < WAL_COUNT; i++) {
+                WAL_FILE_CHANNELS[i] = FileChannel.open(
+                        DIR_ESSD.resolve(i+".log"),
+                        StandardOpenOption.CREATE, StandardOpenOption.WRITE, StandardOpenOption.APPEND
+                );
+            }
+
+            WAL = FileChannel.open(
+                    DIR_ESSD.resolve("wal"),
+                    StandardOpenOption.CREATE, StandardOpenOption.WRITE, StandardOpenOption.APPEND
+                    , StandardOpenOption.DSYNC
+            );
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    private static final Map<String,FileChannel> dbFileChannelMap = new ConcurrentHashMap<>();
+    private static final Map<String,FileChannel> walFileChannelMap = new ConcurrentHashMap<>();
+    private static final AtomicInteger walFileCurrentIndex = new AtomicInteger();
+
+    FileChannel getWalFileChannel(String topic){
+        FileChannel fileChannel = walFileChannelMap.get(topic);
+        if (fileChannel == null){
+            synchronized (this){
+                fileChannel = walFileChannelMap.get(topic);
+                if (fileChannel == null){
+                    int index = walFileCurrentIndex.getAndIncrement();
+                    fileChannel = WAL_FILE_CHANNELS[index % WAL_COUNT];
+                    walFileChannelMap.put(topic,fileChannel);
+                }
+            }
+        }
+        return fileChannel;
+    }
+
+
+
+    @Override
+    public long append(String topic, int queueId, ByteBuffer data) {
+
+        AtomicLong offsetAdder = APPEND_OFFSET_MAP.computeIfAbsent(topic + "+" + queueId, k -> new AtomicLong());
+        long offset = offsetAdder.getAndIncrement();
+        // 更新最大位点
+        // 保存 data 中的数据
+        try {
+            // msg长度
+            ByteBuffer lenBufWrite = ByteBuffer.allocate(Short.BYTES);
+            lenBufWrite.putShort((short) data.limit());
+            lenBufWrite.flip();
+            // 落 wal
+            WAL.write(lenBufWrite);
+            lenBufWrite.rewind(); // 重复读
+            WAL.write(data);
+            data.rewind();
+            // 落具体 queue
+            Path queuePath = DIR_ESSD.resolve(topic);
+            Files.createDirectories(queuePath);
+            FileChannel dataChannel = FileChannel.open(
+                    queuePath.resolve(queueId + ".d"),
+                    StandardOpenOption.CREATE, StandardOpenOption.WRITE, StandardOpenOption.APPEND
+            );
+            long position = dataChannel.position();
+            dataChannel.write(lenBufWrite);
+            lenBufWrite.flip();
+            dataChannel.write(data);
+            dataChannel.close();
+            // 索引
+            if (offset % 128 == 0) {
+                ByteBuffer indexBuf = ByteBuffer.allocate(16);
+                indexBuf.putLong(offset);
+                indexBuf.putLong(position);
+                indexBuf.flip();
+                FileChannel indexChannel = FileChannel.open(
+                        queuePath.resolve(queueId + ".i"),
+                        StandardOpenOption.CREATE, StandardOpenOption.WRITE, StandardOpenOption.APPEND
+                );
+                indexChannel.write(indexBuf);
+                indexChannel.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return offset;
+    }
+
+    @Override
+    public Map<Integer, ByteBuffer> getRange(String topic, int queueId, long offset, int fetchNum) {
+        Map<Integer, ByteBuffer> dataMap = new HashMap<>(fetchNum);
+        Path indexPath = DIR_ESSD.resolve(topic).resolve(queueId + ".i");
+        Path dataPath = DIR_ESSD.resolve(topic).resolve(queueId + ".d");
+        try {
+            long prevOffset = 0;
+            long position = 0;
+            if (Files.exists(indexPath)) {
+                FileChannel indexChannel = FileChannel.open(indexPath, StandardOpenOption.READ);
+                MappedByteBuffer indexMapBuf = indexChannel.map(FileChannel.MapMode.READ_ONLY, 0, indexChannel.size());
+                // 找数据偏移量
+                while (indexMapBuf.hasRemaining()) {
+                    long curOffset = indexMapBuf.getLong();
+                    if (curOffset > offset) {
+                        break;
+                    }
+                    prevOffset = curOffset;
+                    position = indexMapBuf.getLong();
+                }
+                Cleaner cleaner = ((sun.nio.ch.DirectBuffer) indexMapBuf).cleaner();
+                if (cleaner != null) {
+                    cleaner.clean();
+                }
+                indexChannel.close();
+            } else {
+                System.out.println(indexPath + "不存在");
+            }
+            if (Files.exists(dataPath)) {
+                FileChannel dataChannel = FileChannel.open(dataPath, StandardOpenOption.READ);
+                dataChannel.position(position);
+                int key = 0;
+                ByteBuffer lenBufRead = ByteBuffer.allocate(Short.BYTES);
+                while (true) {
+                    if (dataChannel.read(lenBufRead) <= 0) {
+                        break;
+                    }
+                    lenBufRead.flip();
+                    short length = lenBufRead.getShort();
+                    lenBufRead.flip();
+                    ByteBuffer msgBuf = ByteBuffer.allocate(length);
+                    dataChannel.read(msgBuf);
+                    msgBuf.flip();
+                    if (prevOffset >= offset) {
+                        dataMap.put(key, msgBuf);
+                        key++;
+                        if (key == fetchNum) {
+                            break;
+                        }
+                    }
+                    prevOffset++;
+                }
+                dataChannel.close();
+            } else {
+                System.out.println(dataPath + "不存在");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return dataMap;
+    }
+}
---
+++ src/main/java/io/openmessaging/FinkysPage.java
@@ -0,0 +1,126 @@
+package io.openmessaging;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static io.openmessaging.FinkysMessageQueueImpl.DIR_ESSD;
+
+public class FinkysPage {
+
+    private Map<Integer, Long> globalIdOffsetMap = new HashMap<>();
+
+    private String topic;
+
+    private ByteBuffer byteBuffer = ByteBuffer.allocate(32 * 1024 * 1024);
+
+    private List<Map<Integer, Long>> idOffsetMapList = new ArrayList<>();
+
+    private Map<Integer, Long> currentIdOffsetMap = new HashMap<>();
+
+    private FileChannel currentFileChannel;
+
+    public FinkysPage(String topic) {
+        this.topic = topic;
+    }
+
+
+    private Path getFilePath(int index) {
+        return DIR_ESSD.resolve(topic + "_" + index + ".d");
+    }
+
+    public long append(int queueId, ByteBuffer data) throws IOException {
+        if (currentFileChannel == null) {
+            currentFileChannel = FileChannel.open(
+                    getFilePath(idOffsetMapList.size()),
+                    StandardOpenOption.CREATE, StandardOpenOption.WRITE, StandardOpenOption.APPEND
+            );
+        }
+        Long offset = globalIdOffsetMap.get(queueId);
+        if (offset == null) {
+            offset = 0L;
+        } else {
+            offset++;
+        }
+        globalIdOffsetMap.put(queueId, offset);
+        int space = 6 + data.limit();
+        if (byteBuffer.remaining() >= space) {
+            currentIdOffsetMap.put(queueId, offset);
+            byteBuffer.putShort((short) data.limit());
+            byteBuffer.putInt(queueId);
+            byteBuffer.put(data);
+        } else {
+            idOffsetMapList.add(currentIdOffsetMap);
+            currentIdOffsetMap = new HashMap<>();
+            currentFileChannel.write(byteBuffer);
+            currentFileChannel.close();
+            currentFileChannel = null;
+            byteBuffer.clear();
+        }
+        return offset;
+    }
+
+    public Map<Integer, ByteBuffer> getRange(int queueId, long offset, int fetchNum) throws IOException {
+        for (int i = 0; i < idOffsetMapList.size(); i++) {
+            Map<Integer, Long> integerLongMap = idOffsetMapList.get(i);
+            Long mapOffset = integerLongMap.get(queueId);
+            if (mapOffset != null && mapOffset > offset) {
+                return getRangeFromIndex(i - 1, queueId, offset, fetchNum);
+            }
+        }
+        return getRangeFromIndex(idOffsetMapList.size() - 1, queueId, offset, fetchNum);
+    }
+
+    private Map<Integer, ByteBuffer> getRangeFromIndex(int index, int queueId, long offset, int fetchNum) throws IOException {
+        Map<Integer, ByteBuffer> result = new HashMap<>();
+        if (index < 0) {
+            index = 0;
+        }
+        for (int i = index; i < idOffsetMapList.size(); i++) {
+            Map<Integer, Long> integerLongMap = idOffsetMapList.get(i);
+            Long mapOffset = integerLongMap.get(queueId);
+            if (mapOffset != null && mapOffset <= offset + fetchNum - 1) {
+                Path dataPath = getFilePath(i);
+                if (Files.exists(dataPath)) {
+                    FileChannel dataChannel = FileChannel.open(dataPath, StandardOpenOption.READ);
+                    ByteBuffer headBuf = ByteBuffer.allocate(Short.BYTES + Integer.BYTES);
+                    while (true) {
+                        if (dataChannel.read(headBuf) <= 0) {
+                            break;
+                        }
+                        headBuf.flip();
+                        short curLen = headBuf.getShort();
+                        int curQueueId = headBuf.getInt();
+                        headBuf.flip();
+                        if (queueId == curQueueId) {
+                            if (mapOffset >= offset) {
+                                if (mapOffset > offset + fetchNum - 1) {
+                                    break;
+                                }
+                                ByteBuffer msgBuf = ByteBuffer.allocate(curLen);
+                                dataChannel.read(msgBuf);
+                                msgBuf.flip();
+                                result.put(result.size(), msgBuf);
+                            } else {
+                                dataChannel.position(dataChannel.position() + curLen);
+                            }
+                            mapOffset++;
+                        } else {
+                            dataChannel.position(dataChannel.position() + curLen);
+                        }
+
+                    }
+                    dataChannel.close();
+                }
+            }
+        }
+        return result;
+    }
+}
