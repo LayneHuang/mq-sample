0ea67ce834c556d9f2de78511f8ef0e1c591a3ae
leo <915692722@qq.com>
2021-09-24 19:33:34 +0800

    试水

--- src/main/java/io/openmessaging/DefaultMessageQueueImpl.java
+++ src/main/java/io/openmessaging/DefaultMessageQueueImpl.java
@@ -1,18 +1,22 @@
 package io.openmessaging;
 
 import sun.misc.Cleaner;
+import sun.nio.ch.DirectBuffer;
 
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
+import java.nio.channels.FileChannel.MapMode;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicLong;
 
 /**
@@ -26,6 +30,52 @@ public class DefaultMessageQueueImpl extends MessageQueue {
     //    public static final Path DIR_ESSD = Paths.get(System.getProperty("user.dir")).resolve("target");
     public static final ConcurrentHashMap<String, AtomicLong> APPEND_OFFSET_MAP = new ConcurrentHashMap<>();
 
+    public static final Path LOGS_PATH = DIR_ESSD.resolve("log");
+    public static AtomicInteger LOG_ADDER = new AtomicInteger();
+    public static FileChannel LOG_FILE_CHANNEL;
+    public static MappedByteBuffer LOG_MAPPED_BUF;
+
+    static {
+        try {
+            Files.createDirectories(LOGS_PATH);
+            openLog();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public synchronized static ByteBuffer writeLog(ByteBuffer data) throws IOException {
+        if (LOG_MAPPED_BUF.remaining() < data.limit()) {
+            unmap(LOG_MAPPED_BUF);
+            LOG_FILE_CHANNEL.close();
+            openLog();
+        }
+        int position = LOG_MAPPED_BUF.position();
+        LOG_MAPPED_BUF.put(data);
+        LOG_MAPPED_BUF.force();
+        ByteBuffer indexBuf = ByteBuffer.allocate(8);
+        indexBuf.putShort((short) LOG_ADDER.get());
+        indexBuf.putInt(position);
+        indexBuf.putShort((short) data.limit());
+        indexBuf.flip();
+        return indexBuf;
+    }
+
+    public static void openLog() throws IOException {
+        int logNum = LOG_ADDER.getAndIncrement();
+        Path logPath = LOGS_PATH.resolve(String.valueOf(logNum));
+        Files.createFile(logPath);
+        LOG_FILE_CHANNEL = FileChannel.open(logPath, StandardOpenOption.READ, StandardOpenOption.WRITE);
+        LOG_MAPPED_BUF = LOG_FILE_CHANNEL.map(MapMode.READ_WRITE, 0, 1_073_741_824);// 1G
+    }
+
+    public static void unmap(MappedByteBuffer indexMapBuf) throws IOException {
+        Cleaner cleaner = ((DirectBuffer) indexMapBuf).cleaner();
+        if (cleaner != null) {
+            cleaner.clean();
+        }
+    }
+
     @Override
     public long append(String topic, int queueId, ByteBuffer data) {
         String key = (topic + " + " + queueId).intern();
@@ -35,37 +85,22 @@ public class DefaultMessageQueueImpl extends MessageQueue {
         // 保存 data 中的数据
         synchronized (key) {
             try {
-                Path queuePath = DIR_ESSD.resolve(topic);
-                Files.createDirectories(queuePath);
-                FileChannel dataChannel = FileChannel.open(
-                        queuePath.resolve(queueId + ".d"),
-                        StandardOpenOption.CREATE, StandardOpenOption.WRITE, StandardOpenOption.APPEND
-//                    , StandardOpenOption.SYNC
-                );
-//            dataChannel.force(true);
-                long position = dataChannel.position();
-                ByteBuffer lenBufWrite = ByteBuffer.allocate(Short.BYTES);
-                lenBufWrite.putShort((short) data.limit());
-                lenBufWrite.flip();
-                dataChannel.write(lenBufWrite);
-                lenBufWrite.flip();
-                dataChannel.write(data);
-                dataChannel.close();
+                // 落 wal
+                ByteBuffer indexBuf = writeLog(data);
                 // 索引
-                if (offset % 64 == 0) {
-                    ByteBuffer indexBuf = ByteBuffer.allocate(16);
-                    indexBuf.putLong(offset);
-                    indexBuf.putLong(position);
-                    indexBuf.flip();
-                    FileChannel indexChannel = FileChannel.open(
-                            queuePath.resolve(queueId + ".i"),
-                            StandardOpenOption.CREATE, StandardOpenOption.WRITE, StandardOpenOption.APPEND
-//                        , StandardOpenOption.SYNC
-                    );
-//                indexChannel.force(true);
-                    indexChannel.write(indexBuf);
-                    indexChannel.close();
+                Path topicPath = DIR_ESSD.resolve(topic);
+                try {
+                    Files.createDirectories(topicPath);
+                } catch (IOException e) {
                 }
+                FileChannel indexChannel = FileChannel.open(
+                        topicPath.resolve(String.valueOf(queueId)),
+                        StandardOpenOption.CREATE, StandardOpenOption.WRITE, StandardOpenOption.APPEND
+//                            , StandardOpenOption.DSYNC
+                );
+//                indexChannel.force(true);
+                indexChannel.write(indexBuf);
+                indexChannel.close();
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -75,63 +110,50 @@ public class DefaultMessageQueueImpl extends MessageQueue {
 
     @Override
     public Map<Integer, ByteBuffer> getRange(String topic, int queueId, long offset, int fetchNum) {
-        Map<Integer, ByteBuffer> dataMap = new HashMap<>(fetchNum);
+        Map<Integer, ByteBuffer> dataMap = null;
         Path indexPath = DIR_ESSD.resolve(topic).resolve(queueId + ".i");
-        Path dataPath = DIR_ESSD.resolve(topic).resolve(queueId + ".d");
         try {
-            long prevOffset = 0;
-            long position = 0;
             if (Files.exists(indexPath)) {
                 FileChannel indexChannel = FileChannel.open(indexPath, StandardOpenOption.READ);
-                MappedByteBuffer indexMapBuf = indexChannel.map(FileChannel.MapMode.READ_ONLY, 0, indexChannel.size());
-                // 找数据偏移量
-                while (indexMapBuf.hasRemaining()) {
-                    long curOffset = indexMapBuf.getLong();
-                    if (curOffset > offset) {
-                        break;
-                    }
-                    prevOffset = curOffset;
-                    position = indexMapBuf.getLong();
-                }
-                Cleaner cleaner = ((sun.nio.ch.DirectBuffer) indexMapBuf).cleaner();
-                if (cleaner != null) {
-                    cleaner.clean();
-                }
-                indexChannel.close();
-            } else {
-                System.out.println(indexPath + "不存在");
-            }
-            if (Files.exists(dataPath)) {
-                FileChannel dataChannel = FileChannel.open(dataPath, StandardOpenOption.READ);
-                dataChannel.position(position);
-                int key = 0;
-                ByteBuffer lenBufRead = ByteBuffer.allocate(Short.BYTES);
-                while (true) {
-                    if (dataChannel.read(lenBufRead) <= 0) {
-                        break;
-                    }
-                    lenBufRead.flip();
-                    short length = lenBufRead.getShort();
-                    lenBufRead.flip();
-                    ByteBuffer msgBuf = ByteBuffer.allocate(length);
-                    dataChannel.read(msgBuf);
-                    msgBuf.flip();
-                    if (prevOffset >= offset) {
+                long fileSize = indexChannel.size();
+                long start = offset * 6;
+                if (start < fileSize) {
+                    dataMap = new HashMap<>(fetchNum);
+                    int key = 0;
+                    long end = Math.min(start + fetchNum * 6L, fileSize);
+                    long mappedSize = end - start;
+                    MappedByteBuffer indexMappedBuf = indexChannel.map(FileChannel.MapMode.READ_ONLY, start, mappedSize);
+                    short logNum, msgLen;
+                    long position;
+                    Path logPath;
+                    FileChannel logChannel;
+                    while (indexMappedBuf.hasRemaining()) {
+                        logNum = indexMappedBuf.getShort();
+                        position = indexMappedBuf.getInt();
+                        msgLen = indexMappedBuf.getShort();
+                        logPath = LOGS_PATH.resolve(String.valueOf(logNum));
+                        logChannel = FileChannel.open(logPath, StandardOpenOption.READ);
+//                        MappedByteBuffer msgMappedBuf = logChannel.map(FileChannel.MapMode.READ_ONLY, position, msgLen);
+                        ByteBuffer msgBuf = ByteBuffer.allocate(msgLen);
+                        logChannel.read(msgBuf, position);
+                        logChannel.close();
+                        msgBuf.flip();
                         dataMap.put(key, msgBuf);
                         key++;
-                        if (key == fetchNum) {
-                            break;
-                        }
                     }
-                    prevOffset++;
+                    unmap(indexMappedBuf);
+                    indexChannel.close();
                 }
-                dataChannel.close();
             } else {
-                System.out.println(dataPath + "不存在");
+                System.out.println(indexPath + "不存在");
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return dataMap;
+        if (dataMap != null) {
+            return dataMap;
+        } else {
+            return Collections.emptyMap();
+        }
     }
 }
