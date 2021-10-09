db2d4ab0a9824590a3809ed39f1ffeba1e8cc6f7
LayneHuang <hl_mark@163.com>
2021-10-09 16:45:56 +0800

    重启后插入会报错

--- src/main/java/io/openmessaging/PerformanceTester.java
+++ src/main/java/io/openmessaging/PerformanceTester.java
@@ -25,12 +25,16 @@ public class PerformanceTester {
         long start = System.currentTimeMillis();
         String topic = "topic1";
         int queueId = 1;
+        long queryOffset = 0;
+        int queryNum = 500;
         for (; i < 10000; i++) {
             String text = String.valueOf(i);
             ByteBuffer buf = ByteBuffer.wrap(text.getBytes(StandardCharsets.UTF_8));
             long offset = messageQueue.append(topic, queueId, buf);
             targetMap.put(toKey(topic, queueId, offset), buf);
+            if (offset > queryNum) queryOffset = offset - queryNum;
         }
+
         Thread threadW1 = new Thread(() -> {
             for (; i < 20000; i++) {
                 String text = String.valueOf(i);
@@ -39,23 +43,18 @@ public class PerformanceTester {
                 targetMap.put(toKey(topic, queueId, offset), buf);
             }
         });
-        Thread threadR1 = new Thread(() -> {
-            long queryOffset = 19500;
-            int queryNum = 500;
-            Map<Integer, ByteBuffer> ansMap = messageQueue.getRange(topic, queueId, queryOffset, queryNum);
-            for (int i = 0; i < queryNum; ++i) {
-                if (!(new String(targetMap.get(toKey(topic, queueId, queryOffset + i)).array())).equals(
-                        new String(ansMap.get(i).array())
-                )) {
-                    log.info("FUCK YOU");
-                }
+
+        Map<Integer, ByteBuffer> ansMap = messageQueue.getRange(topic, queueId, queryOffset, queryNum);
+        for (int i = 0; i < queryNum; ++i) {
+            if (!(new String(targetMap.get(toKey(topic, queueId, queryOffset + i)).array())).equals(
+                    new String(ansMap.get(i).array())
+            )) {
+                log.info("FUCK YOU");
             }
-            log.info("FINISH-1");
-        });
+        }
+        log.info("FINISH-1");
         threadW1.start();
         threadW1.join();
-        threadR1.start();
-        threadR1.join();
         log.info("cost: {}", System.currentTimeMillis() - start);
     }
 
