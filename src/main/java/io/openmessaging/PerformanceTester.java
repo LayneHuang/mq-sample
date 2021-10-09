c255a561b17854668347c25386047abcaddcf729
LayneHuang <hl_mark@163.com>
2021-10-09 16:37:05 +0800

    重启后插入会报错

--- src/main/java/io/openmessaging/PerformanceTester.java
+++ src/main/java/io/openmessaging/PerformanceTester.java
@@ -7,6 +7,7 @@ import org.slf4j.LoggerFactory;
 import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class PerformanceTester {
     private static final Logger log = LoggerFactory.getLogger(PerformanceTester.class);
@@ -19,27 +20,37 @@ public class PerformanceTester {
 
     private static void gao() throws InterruptedException {
         MessageQueue messageQueue = new LeoMessageQueueImpl();
+        Map<String, ByteBuffer> targetMap = new ConcurrentHashMap<>();
+
         long start = System.currentTimeMillis();
-        Map<Integer, ByteBuffer> range;
+        String topic = "topic1";
+        int queueId = 1;
         for (; i < 10000; i++) {
             String text = String.valueOf(i);
             ByteBuffer buf = ByteBuffer.wrap(text.getBytes(StandardCharsets.UTF_8));
-            messageQueue.append("topicA", 1, buf);
+            long offset = messageQueue.append(topic, queueId, buf);
+            targetMap.put(toKey(topic, queueId, offset), buf);
         }
         Thread threadW1 = new Thread(() -> {
             for (; i < 20000; i++) {
                 String text = String.valueOf(i);
                 ByteBuffer buf = ByteBuffer.wrap(text.getBytes(StandardCharsets.UTF_8));
-                messageQueue.append("topicA", 1, buf);
+                long offset = messageQueue.append(topic, queueId, buf);
+                targetMap.put(toKey(topic, queueId, offset), buf);
             }
         });
         Thread threadR1 = new Thread(() -> {
-            messageQueue.getRange("topicA", 1, 19500, 500);
-            System.out.println("FINISH");
-            log.info("边写边查 threadR1");
-            check(messageQueue.getRange("topicA", 1, 9950, 100));
+            long queryOffset = 19500;
+            int queryNum = 500;
+            Map<Integer, ByteBuffer> ansMap = messageQueue.getRange(topic, queueId, queryOffset, queryNum);
+            for (int i = 0; i < queryNum; ++i) {
+                if (!(new String(targetMap.get(toKey(topic, queueId, queryOffset + i)).array())).equals(
+                        new String(ansMap.get(i).array())
+                )) {
+                    log.info("FUCK YOU");
+                }
+            }
             log.info("FINISH-1");
-
         });
         threadW1.start();
         threadW1.join();
@@ -48,13 +59,28 @@ public class PerformanceTester {
         log.info("cost: {}", System.currentTimeMillis() - start);
     }
 
+    private static String toKey(String topic, int queueId, long offset) {
+        return topic + "-" + queueId + "-" + offset;
+    }
+
+    private static String toTopic(String key) {
+        return key.split("-")[0];
+    }
+
+    private static int toQueueId(String key) {
+        return Integer.parseInt(key.split("-")[1]);
+    }
+
+    private static long toOffset(String key) {
+        return Integer.parseInt(key.split("-")[2]);
+    }
+
     private static void check(Map<Integer, ByteBuffer> map) {
         map.forEach((key, value) -> {
             String s = new String(value.array());
             if (!String.valueOf(key).equals(s)) {
                 log.debug("FUCK: {}, {}", key, s);
             }
-//            log.debug("TRUE: {}, {}", key, s);
         });
     }
 }
