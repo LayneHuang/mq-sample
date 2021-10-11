57c5eb38c4799b66564833297700437bff2413d7
LayneHuang <hl_mark@163.com>
2021-10-11 10:08:46 +0800

    慢慢调试

--- src/main/java/io/openmessaging/Constant.java
+++ src/main/java/io/openmessaging/Constant.java
@@ -20,7 +20,7 @@ public class Constant {
 
     public static final String DIR_PMEM = "/pmem";
 
-    public static final int WAL_FILE_COUNT = 5;
+    public static final int WAL_FILE_COUNT = 10;
 
     public static final int WRITE_SIZE = 8 * 1024;
 
